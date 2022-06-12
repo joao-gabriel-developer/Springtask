a4bdb9b528fb41f02e4ff210c3793b432399214d
joao-gabriel-developer <joaogabrieloprogramador@gmail.com>
2022-06-12 00:22:34 -0300

    Create project Task Manager 2
    Beggin of migration to Task Manager in python through Django for Java through Spring with Springboot and Spring Web

---
+++ Springtasks/src/main/java/com/taskmanager/Springtasks/SpringtasksApplication.java
@@ -0,0 +1,13 @@
+package com.taskmanager.Springtasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtasksApplication.class, args);
+	}
+
+}
---
+++ Springtasks/src/test/java/com/taskmanager/Springtasks/SpringtasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskmanager.Springtasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
