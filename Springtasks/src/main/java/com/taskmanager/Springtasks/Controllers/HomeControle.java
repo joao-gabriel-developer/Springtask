1489fd9ccbb11426583fe231e0bf78fb05861061
joao-gabriel-developer <joaogabrieloprogramador@gmail.com>
2022-06-14 18:33:47 -0300

    Testing template variable system of thymeleaf

--- Springtasks/src/main/java/com/taskmanager/Springtasks/Controllers/HomeControle.java
+++ Springtasks/src/main/java/com/taskmanager/Springtasks/Controllers/HomeControle.java
@@ -1,12 +1,17 @@
 package com.taskmanager.Springtasks.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.stereotype.Controller;
 
 @Controller
 public class HomeControle {
     @GetMapping("/")
-    public String tasks1(){
+    public String tasks1(Model model){
+        model.addAttribute("dateofcreation", "Fourtheen of June of Two Thousand Twenty Two");
+
+
         return "Tasks1/tasklist.html";
     }
     
