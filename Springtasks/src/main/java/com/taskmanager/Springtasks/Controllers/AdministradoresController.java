4111a6ab42ebc01013322999c2b5c2115f241601
joao-gabriel-developer <joaogabrieloprogramador@gmail.com>
2022-06-23 20:44:48 -0300

    Make the endpoints of application

--- Springtasks/src/main/java/com/taskmanager/Springtasks/Controllers/AdministradoresController.java
+++ Springtasks/src/main/java/com/taskmanager/Springtasks/Controllers/AdministradoresController.java
@@ -3,12 +3,15 @@ package com.taskmanager.Springtasks.Controllers;
 
 
 
+
 import java.util.List;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+//import com.taskmanager.Springtasks.Controllers.Administrador;
 
 import com.taskmanager.Springtasks.repositorio.AdministradoresRepo;
 
@@ -19,13 +22,20 @@ public class AdministradoresController {
     @Autowired
     private AdministradoresRepo repo;
 
+    //List administradores = new ArrayList<>();
+
 
     @GetMapping("/administradores")
     public String index(Model model){
 
+
         // @SuppressWarnings("unchecked")
-        // List<Administrador> administradores = (List<Administrador>)repo.findAll();
-        List<Administrador> administradores = (List<Administrador>)repo.findAll();
+        //dministradores =  List<Administrador>repo.findAll();
+        //List<Administrador> administradores = (List<Administrador>)repo.findAll();
+        @SuppressWarnings("rawtypes")
+        List administradores = repo.findAll();
+        
+
         model.addAttribute("Administradores", administradores);
 
         return "administradores/index";
--- Springtasks/src/main/java/com/taskmanager/Springtasks/repositorio/AdministradoresRepo.java
+++ Springtasks/src/main/java/com/taskmanager/Springtasks/repositorio/AdministradoresRepo.java
@@ -1,9 +1,17 @@
 package com.taskmanager.Springtasks.repositorio;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.taskmanager.Springtasks.Models.Administrador;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AdministradoresRepo extends CrudRepository<Administrador, Integer> {
     
+    
+    @Override
+    List<Administrador> findAll();
+
 }
